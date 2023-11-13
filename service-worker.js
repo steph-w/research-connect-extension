96183fbbef9acc17739b93eeee65658b2b5bb280
Stephanie Wang <steph17@alumni.stanford.edu>
2023-11-12 20:18:46 -0500

    cleanup

--- service-worker.js
+++ service-worker.js
@@ -20,26 +20,7 @@ chrome.sidePanel
   .setPanelBehavior({ openPanelOnActionClick: true })
   .catch((error) => console.error(error));
 
-// chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
-//   if (!tab.url) return;
-//   const url = new URL(tab.url);
-//   // Enables the side panel on google.com
-//   if (url.origin === GOOGLE_ORIGIN) {
-//     console.log("GOOGLE_ORIGIN SET!!")
-//     await chrome.sidePanel.setOptions({
-//       tabId,
-//       path: 'sidepanel.html',
-//       enabled: true
-//     });
-//   } else {
-//     // Disables the side panel on all other sites
-//     await chrome.sidePanel.setOptions({
-//       tabId,
-//       enabled: false
-//     });
-//   }
-// });
-
+// Allows changing the panel html.
 chrome.runtime.onMessage.addListener(async (message, sender) => {
   if (message.type === 'open_studies_page') {
     await chrome.sidePanel.setOptions({ 
